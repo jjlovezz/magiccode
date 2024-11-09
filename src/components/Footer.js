eb01fe32446c113472cc3f01abd7d2c720043a7b
MagicCodeMaggie <maggie.zxu@outlook.com>
2024-11-09 22:54:08 +1100

    footer delete useless info and add wechat official account

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,68 +4,71 @@ import { Container, Row, Col } from 'react-bootstrap';
 function Footer() {
     return (
         <>
-           
-        <Container id='contact'>
-            <Row className='footer'>
-                <Col xs={6} lg={3} className='footerBox'>
-                    <div className='footMain'>
-                        <i className="fab fa-digital-ocean footLogo"></i>
-                        
-                    </div>
-                    
+
+            <Container id='contact'>
+                <Row className='footer'>
+
+                    <Col xs={6} lg={3} className='footerBox'>
+                        <div className='footTitle'>
+                            <h4>Social Media</h4>
+                        </div>
+                        <ul className='footUl'>
+                            <li>Rednote</li>
+                            <li>Youtube</li>
+                            <li>Facebook</li>
+                        </ul>
+                    </Col>
+                    <Col xs={6} lg={3} className='footerBox'>
+                        <div className='footTitle'>
+                            <h4>About us</h4>
+                        </div>
+                        <ul className='footUl'>
+                            <li>Our school</li>
+                            <li>Our Teachers</li>
+                            <li>Our Courses</li>
+                        </ul>
+                    </Col>
+                    <Col xs={6} lg={3} className='footerBox'>
+                        <div className='footTitle'>
+                            <h4>Contact us</h4>
+                        </div>
+
                         <div className='footmail'>
                             <p className="footLink">
                                 Wechat: MagicCodeEdu
-                                <img src="./wechat.jpg" alt="Wechat Code" width="170px" height="170px"/>
+                                <img src="./wechat.jpg" alt="Wechat Code" width="170px" height="170px" />
                             </p>
                         </div>
-                    
-                    <div className='footmail'>
-                        <p>Email: <a className="footLink" href="mailto:magiccodeedu@outlook.com">magiccodeedu@outlook.com</a>
-                        </p>
-                    </div>
-                    
 
-                </Col>
-                <Col xs={6} lg={3} className='footerBox'>
-                    <div className='footTitle'>
-                        <h4>Social Media</h4>
-                    </div>
-                    <ul className='footUl'>
-                        <li>Instagram</li>
-                        <li>Facebook</li>
-                        <li>Youtube</li>
-                        <li>Twitter</li>
-                    </ul>
-                </Col>
-                <Col xs={6} lg={3} className='footerBox'>
-                    <div className='footTitle'>
-                        <h4>About us</h4>
-                    </div>
-                    <ul className='footUl'>
-                        <li>Our school</li>
-                        <li>Teachers</li>
-                        <li>Program</li>
-                        <li>Price</li>
-                        <li>Ivestors</li>
-                    </ul>
-                </Col>
-                <Col xs={6} lg={3} className='footerBox'>
-                    <div className='footTitle'>
-                        <h4>Contact us</h4>
-                    </div>
-                    <ul className='footUl'>
-                        <li>Contact</li>
-                        <li>Support</li>
-                        <li>Franchise</li>
-                        <li>Sponsorshps</li>
-                        
-                    </ul>
-                </Col>
-                
-            </Row>
-        </Container>
-            
+                        <div className='footmail'>
+                            <p>Email: <a className="footLink" href="mailto:magiccodeedu@gmail.com">magiccodeedu@gmail.com</a>
+                            </p>
+                        </div>
+
+
+                    </Col>
+                    <Col xs={6} lg={3} className='footerBox'>
+                        <div className='footTitle'>
+                            <h4>More Information</h4>
+                        </div>
+
+                        <div className='footmail'>
+                            <p className="footLink">
+                                Wechat Official Account
+                                <img src="./gongzhonghao.jpg" alt="Wechat Official Account" width="170px" height="170px" />
+                            </p>
+                        </div>
+
+                        <div className='footmail'>
+                            <p>Scan the code to know more...
+                            </p>
+                        </div>
+
+
+                    </Col>
+                </Row>
+            </Container>
+
         </>
     )
 }
