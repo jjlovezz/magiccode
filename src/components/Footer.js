25e533c68cff490a9fcba632958819c555b85454
MagicCodeMaggie <maggie.zxu@outlook.com>
2024-11-09 23:07:38 +1100

    delete useless info in footer, only two code left.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,26 +10,6 @@ function Footer() {
 
                     <Col xs={6} lg={3} className='footerBox'>
                         <div className='footTitle'>
-                            <h4>Social Media</h4>
-                        </div>
-                        <ul className='footUl'>
-                            <li>Rednote</li>
-                            <li>Youtube</li>
-                            <li>Facebook</li>
-                        </ul>
-                    </Col>
-                    <Col xs={6} lg={3} className='footerBox'>
-                        <div className='footTitle'>
-                            <h4>About us</h4>
-                        </div>
-                        <ul className='footUl'>
-                            <li>Our school</li>
-                            <li>Our Teachers</li>
-                            <li>Our Courses</li>
-                        </ul>
-                    </Col>
-                    <Col xs={6} lg={3} className='footerBox'>
-                        <div className='footTitle'>
                             <h4>Contact us</h4>
                         </div>
 
@@ -47,9 +27,10 @@ function Footer() {
 
 
                     </Col>
+
                     <Col xs={6} lg={3} className='footerBox'>
                         <div className='footTitle'>
-                            <h4>More Information</h4>
+                            <h4>About us</h4>
                         </div>
 
                         <div className='footmail'>
@@ -60,12 +41,15 @@ function Footer() {
                         </div>
 
                         <div className='footmail'>
-                            <p>Scan the code to know more...
+                            <p>
                             </p>
                         </div>
 
 
                     </Col>
+
+
+
                 </Row>
             </Container>
 
