fdd95d43d82bbc362be4a73b014d5d00433c5f49
MagicCodeMaggie <maggie.zxu@outlook.com>
2024-11-11 10:00:00 +1100

    change the reviews to google reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import {Container, Row} from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import 'swiper/swiper.scss';
 import 'swiper/swiper-bundle.min.css';
@@ -10,7 +10,8 @@ function Reviews() {
     return (
         <>
             <Container fluid className='coursesBlock'>
-            
+                <p>The information is sourced from Google Maps reviews written by the parents of our students.</p>
+
                 <Container>
                     <Row className='headcomp'>
 
@@ -21,77 +22,124 @@ function Reviews() {
                                 0: {
                                     // width: 340,
                                     slidesPerView: 1,
-                                  },
+                                },
                                 768: {
-                                //   width: 768,
-                                  slidesPerView: 1,
+                                    //   width: 768,
+                                    slidesPerView: 1,
                                 },
                                 1140: {
                                     // width: 1140,
                                     slidesPerView: 2,
-                                  },
- 
-                              }}
+                                },
+
+                            }}
                             autoplay={{ delay: 3000 }}
 
-                            >
+                        >
                             <SwiperSlide>
-                            
-                            <div className='review'>
-                                <div className='review-text'>
-                                My son James is 13 years old. He recently became interested in programming, so I gave him a basic programming course for his birthday. James is delighted with the lessons and his teacher! He is looking forward to every lesson, and even at home he spends a lot of time working on his own projects
-                                </div>
 
-                                <div className='reviewPerson'>
-                                    <div className='reviewPersonAvatar'><img alt='client Nicolas' src='./ava1.jpg'></img></div>
-                                    <div className='reviewPersonInfo'>
-                                        <div className='reviewPersonName'>Nicolas</div>
-                                        <div className='reviewPersonJob'>Worker</div>
+                                <div className='review'>
+                                    <div className='review-text'>
+                                        My daughters had an amazing experience with this coding class! The teacher Maggie is knowledgeable, patient, and making learning complex concepts so easy to understand. The lessons are well-structured, and the hands-on projects really helped the girls solidify what they are learning. They came into the class with zero experience, and now they are confident in the coding abilities. Whether you're a beginner or looking to enhance your skills, MagicCode Education is perfect. Highly recommend to anyone interested in coding!
+                                    </div>
+
+                                    <div className='reviewPerson'>
+                                        <div className='reviewPersonAvatar'><img alt='Client Jenny' src='./client_jenny.png'></img></div>
+                                        <div className='reviewPersonInfo'>
+                                            <div className='reviewPersonName'>Jenny</div>
+                                            {
+                                                //<div className='reviewPersonJob'>Manager</div>
+                                            }
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            
+
                             </SwiperSlide>
                             <SwiperSlide>
-                            
-                            <div className='review'>
-                                <div className='review-text'>
-                                After the trial class, my daughter didn’t want to go home - she liked the class so much! I’m sure the teachers will strengthen her interest in programming and help her achieve academic success. Thanks MagicCode! 
-                                </div>
 
-                                <div className='reviewPerson'>
-                                    <div className='reviewPersonAvatar'><img alt='client Lily' src='./ava3.jpg'></img></div>
-                                    <div className='reviewPersonInfo'>
-                                        <div className='reviewPersonName'>Lily</div>
-                                        <div className='reviewPersonJob'>Nurse</div>
+                                <div className='review'>
+                                    <div className='review-text'>
+                                        Highly recommended! Teacher Maggie is very serious and responsible, and the class is lively and interesting. The children especially like the courses arranged by the teacher. They can complete a project every time they attend class, which gives the children a special sense of accomplishment.
+                                    </div>
+
+                                    <div className='reviewPerson'>
+                                        <div className='reviewPersonAvatar'><img alt='Client Suqiong' src='./client_suqiong.png'></img></div>
+                                        <div className='reviewPersonInfo'>
+                                            <div className='reviewPersonName'>Suqiong</div>
+
+                                            {
+                                                //<div className='reviewPersonJob'>Manager</div>
+                                            }
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            
+
                             </SwiperSlide>
                             <SwiperSlide>
-                                
-                            <div className='review'>
-                                <div className='review-text'>
-                                My son is a student of the Python course, he really likes it. So his choice of summer holidays was obvious, and I signed him up for the Summer Coding Camp. The son had a great time, he returned home with a lot of impressions and new skills, made new friends. Our family is very happy! 
-                                </div>
 
-                                <div className='reviewPerson'>
-                                    <div className='reviewPersonAvatar'><img alt='client Jenny' src='./ava2.jpg'></img></div>
-                                    <div className='reviewPersonInfo'>
-                                        <div className='reviewPersonName'>Jenny</div>
-                                        <div className='reviewPersonJob'>Manager</div>
+                                <div className='review'>
+                                    <div className='review-text'>
+                                        My son likes coding and has been studying at Magic Code for a year. He particularly likes the three-day holiday class and will definitely come back next term break!
+                                    </div>
+
+                                    <div className='reviewPerson'>
+                                        <div className='reviewPersonAvatar'><img alt='Client Christal' src='./client_christal.png'></img></div>
+                                        <div className='reviewPersonInfo'>
+                                            <div className='reviewPersonName'>Christal</div>
+
+                                            {
+                                                //<div className='reviewPersonJob'>Manager</div>
+                                            }
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            
+
+                            </SwiperSlide>
+                            <SwiperSlide>
+
+                                <div className='review'>
+                                    <div className='review-text'>
+                                        My daughter has been taking classes at Magic Code for several years. She loves both the online classes and the 3-day course during the school holidays.
+                                    </div>
+
+                                    <div className='reviewPerson'>
+                                        <div className='reviewPersonAvatar'><img alt='Client Echo' src='./client_echo.png'></img></div>
+                                        <div className='reviewPersonInfo'>
+                                            <div className='reviewPersonName'>Echo</div>
+                                            {
+                                                //<div className='reviewPersonJob'>Worker</div>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </SwiperSlide>
+                            <SwiperSlide>
+
+                                <div className='review'>
+                                    <div className='review-text'>
+                                        My both kids attended three days holiday program. Absolutely love it! Definitely will come back next term break! Maggie and Jimmy both are super nice and approachable! Keep up the wonderful work!
+                                    </div>
+
+                                    <div className='reviewPerson'>
+                                        <div className='reviewPersonAvatar'><img alt='Client Lin' src='./client_lin.png'></img></div>
+                                        <div className='reviewPersonInfo'>
+                                            <div className='reviewPersonName'>Lin</div>
+                                            {
+                                                //<div className='reviewPersonJob'>Nurse</div>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+
                             </SwiperSlide>
 
-                            
-                            </Swiper>
-                        
+                        </Swiper>
+
                     </Row>
+
                 </Container>
+
             </Container>
         </>
     )
